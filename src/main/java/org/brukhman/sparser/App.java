47b62ed3f60213deeb290741e50d70d2d9fde1b0
Jake Brukhman <jbrukh@gmail.com>
2010-12-22 20:48:29 -0500

    First commit.

---
+++ src/main/java/org/brukhman/sparser/App.java
@@ -0,0 +1,13 @@
+package org.brukhman.sparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
